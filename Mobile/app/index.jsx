712ef49b6ea5cb7b439cb61f241697596ff9ebbc
Rinali Pesakya <rinalipesakya@gmail.com>
2026-02-28 22:26:14 +0530

    Build the UI of the Login Screen

--- Mobile/app/index.jsx
+++ Mobile/app/index.jsx
@@ -1,10 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
 import {Image} from "expo-image";
+import { Link } from "expo-router"; 
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello</Text>
+
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
+
     </View>
   );
 }
